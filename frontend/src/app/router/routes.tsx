fd1e6d88918c2befeaa817164f15ca56f2c1e66d
Letusan <aflahrizqullahn@gmail.com>
2026-01-29 10:13:18 +0700

    -

--- frontend/src/app/router/routes.tsx
+++ frontend/src/app/router/routes.tsx
@@ -6,6 +6,7 @@ import DashboardPage from "../../pages/admin/DashboardPage";
 import ArticlesAdminPage from "../../pages/admin/ArticleAdminPage";
 import CategoriesAdminPage from "../../pages/admin/CategoriesAdminPage";
 import TagsAdminPage from "../../pages/admin/TagsAdminPage";
+import LoginPage from "../../pages/public/LoginAdminPage";
 
 export default function RoutesApp() {
   return (
@@ -18,6 +19,7 @@ export default function RoutesApp() {
       <Route path="/admin/articles" element={<ArticlesAdminPage />} />
       <Route path="/admin/categories" element={<CategoriesAdminPage />} />
       <Route path="/admin/tags" element={<TagsAdminPage />} />
+      <Route path="/admin/login" element={<LoginPage />} />
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
