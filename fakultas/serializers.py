984dad7023957323c98180376347182195cd6474
NabilAflahR <aflahrizqullahn@gmail.com>
2026-02-20 22:58:08 +0700

    cms

--- fakultas/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Fakultas
-
-class FakultasSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Fakultas
-        fields = '__all__'
--- mata_kuliah/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import MataKuliah
-# Register your models here.
-admin.site.register(MataKuliah)
